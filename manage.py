fd903a6f1c136eb92f7d99da6667ec32ebcc6e17
Manthan Vaghasiya <mvaghasiya1412@gmail.com>
2017-04-23 19:34:20 +0530

    Manage.py changed

--- manage.py
+++ manage.py
@@ -6,10 +6,5 @@ if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "front.settings")
 
     from django.core.management import execute_from_command_line
-    # print(sys.argv)
-    a = sys.argv
-    l = len(a)
-    if l < 2:
-    	a.append('runserver')
-    print(a)
-    execute_from_command_line(a)
+
+    execute_from_command_line(sys.argv)
